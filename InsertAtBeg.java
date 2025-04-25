c1b9461cbb01d3a4fef83213a9b6f03e52808336
sainikith07 <155831198+sainikith07@users.noreply.github.com>
2025-04-25 14:13:26 +0530

    Create InsertAtBegining

---
+++ InsertAtBeg.java
@@ -0,0 +1,30 @@
+/*Definition for singly Linked List
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+*/
+
+class Solution {
+    public ListNode insertAtHead(ListNode head, int X) {
+        ListNode nn=new ListNode(X);
+        nn.next=head;
+        head=nn;
+        return head;
+    }
+}
